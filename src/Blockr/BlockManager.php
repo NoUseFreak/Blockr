3fc087fc3c4a8e399a68cfe9fa94874ad5b5ec2e
Dries De Peuter <dries.depeuter@gmail.com>
2013-02-28 22:53:14 +0100

    Add more functionality to the manager.

--- src/Blockr/BlockManager.php
+++ src/Blockr/BlockManager.php
@@ -10,15 +10,113 @@
 
 namespace Blockr;
 
+use Blockr\Block\BlockInterface;
+
 class BlockManager
 {
+    /**
+     * @var \SplObjectStorage
+     */
+    protected $blocks;
+
+    /**
+     * @var array Holds the mapping between the objects and the block identifiers.
+     */
+    protected $blockHashes = array();
+
+    /**
+     * @var BlockQueue
+     */
     protected $queue;
 
-    public function getQueue()
+    /**
+     * Construct all containers.
+     */
+    public function __construct()
     {
-        if (is_null($this->queue)) {
-            $this->queue = new BlockQueue();
+        $this->blocks = new \SplObjectStorage();
+        $this->queue = new BlockQueue();
+    }
+
+    /**
+     * Add a block to the manager.
+     *
+     * @param Block\BlockInterface $block
+     * @param int $priority
+     *
+     * @return bool
+     *
+     * @throws \Exception
+     */
+    public function add(BlockInterface $block, $priority = 0)
+    {
+        if (!$block->getId()) {
+            throw new \Exception('Can\'t add a block without identifier.');
         }
-        return $this->queue;
+
+        $this->blocks->attach($block, array(
+            'priority' => $priority,
+        ));
+        $this->blockHashes[$block->getId()] = $block;
+
+        return true;
+    }
+
+    /**
+     * Check if a block is present for this identifier.
+     *
+     * @param string|int $id
+     *
+     * @return bool
+     */
+    public function hasId($id)
+    {
+        return isset($this->blockHashes[$id]);
+    }
+
+    /**
+     * Check if a block is in the manager.
+     *
+     * @param Block\BlockInterface $block
+     *
+     * @return bool
+     */
+    public function has(BlockInterface $block)
+    {
+        return in_array($block, $this->blockHashes);
+    }
+
+    /**
+     * Remove a block giving the identifier.
+     *
+     * @param string|int $id
+     */
+    public function removeById($id)
+    {
+        $this->blocks->detach($this->blockHashes[$id]);
+        unset($this->blockHashes[$id]);
+    }
+
+    /**
+     * Remove a block from the manager.
+     *
+     * @param Block\BlockInterface $block
+     */
+    public function remove(BlockInterface $block)
+    {
+        $this->blocks->detach($block);
+        unset($this->blockHashes[array_search($block, $this->blockHashes)]);
+    }
+
+    /**
+     * Get a block by a given identifier.
+     *
+     * @param $id
+     *
+     * @return mixed
+     */
+    public function getById($id)
+    {
+        return $this->blockHashes[$id];
     }
 }
--- tests/BlockManagerTest.php
+++ tests/BlockManagerTest.php
@@ -10,6 +10,7 @@
 
 namespace Tests\Blockr;
 
+use Blockr\Block\SimpleBlock;
 use Blockr\BlockManager;
 use Blockr\BlockQueue;
 
@@ -30,8 +31,86 @@ class BlockManagerTest extends \PHPUnit_Framework_TestCase
         unset($this->manager);
     }
 
-    public function testQueue()
+    public function testAddException()
     {
-        $this->assertEquals(new BlockQueue(), $this->manager->getQueue());
+        $this->setExpectedException('Exception');
+
+        $block = new SimpleBlock();
+        $this->manager->add($block);
     }
+
+    public function testAdd()
+    {
+        $block = new SimpleBlock();
+        $block->setId('test');
+
+        $this->assertEquals(true, $this->manager->add($block));
+    }
+
+    public function testGet()
+    {
+        $block = new SimpleBlock();
+        $block->setId('test');
+
+        $this->manager->add($block);
+
+        $this->assertEquals($block, $this->manager->getById('test'));
+    }
+
+    public function testHasId()
+    {
+        $block = new SimpleBlock();
+        $block->setId('test');
+
+        $this->manager->add($block);
+
+        $this->assertEquals(true, $this->manager->hasId('test'));
+        $this->assertEquals(false, $this->manager->hasId('test2'));
+    }
+
+    public function testRemoveById()
+    {
+        $block = new SimpleBlock();
+        $block->setId('test');
+
+        $this->manager->add($block);
+
+        $this->assertEquals($block, $this->manager->getById('test'));
+    }
+
+    public function testHas()
+    {
+        $block = new SimpleBlock();
+        $block->setId('test');
+        $block2 = new SimpleBlock();
+        $block2->setId('test2');
+
+        $this->manager->add($block);
+
+        $this->assertEquals(true, $this->manager->has($block));
+        $this->assertEquals(false, $this->manager->has($block2));
+    }
+
+    public function testRemove()
+    {
+        $block = new SimpleBlock();
+        $block->setId('test');
+
+        $this->manager->add($block);
+        $this->manager->remove($block);
+
+        $this->assertEquals(false, $this->manager->has($block));
+    }
+
+    public function testRemoveId()
+    {
+        $block = new SimpleBlock();
+        $block->setId('test');
+
+        $this->manager->add($block);
+        $this->manager->removeById('test');
+
+        $this->assertEquals(false, $this->manager->has($block));
+    }
+
 }
